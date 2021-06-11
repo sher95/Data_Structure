dcdd87d3d61cbafe83ce4b9d17b8af88621d89b3
Sherzod Mahmudov <sher95c@gmail.com>
2021-06-11 09:45:04 +0500

    itertools

--- itertool.py
+++ itertool.py
@@ -1,4 +1,4 @@
-from itertools import product, permutations, combinations
+from itertools import product, permutations, combinations, accumulate, groupby
 
 # product
 a = [1, 2]
@@ -12,3 +12,26 @@ perm = permutations(c)
 print(list(perm))  # will print [(1, 2, 3), (1, 3, 2), (2, 1, 3), (2, 3, 1), (3, 1, 2), (3, 2, 1)]
 
 # combination
+d = [1, 2, 3, 4]
+comb = combinations(d, 2)
+print(list(comb))  # will print [(1, 2), (1, 3), (1, 4), (2, 3), (2, 4), (3, 4)]
+
+# accumulate----add each other with result
+# olso we can add module operator and multiplay to each other
+# acc = accumulate(ac, func=operator.mul)
+ac = [1, 2, 3, 4]
+acc = accumulate(ac)
+print(a)
+print(list(acc))  # will print [1, 3, 6, 10]
+
+
+# grouby
+
+def smaller(x):
+    return x < 3
+
+
+gr = [1, 2, 3, 4]
+grop = groupby(gr, key=smaller)
+for key, value in grop:
+    print(key, list(value))
